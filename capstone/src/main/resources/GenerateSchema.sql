7b8fb471d4cf8558fbca52c95a84940365daab4b
Andrew Boyd <andrew.boyd@hcl.com>
2021-04-06 11:57:27 -0400

    Added sql initialization script

---
+++ capstone/src/main/resources/GenerateSchema.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE musicstore;
+USE musicstore;
+
+CREATE TABLE s_user(
+	username varchar(255),
+	password varchar(255),
+	user_id int
+);
+
+CREATE TABLE album(
+	album_id int NOT NULL,
+	title varchar(255),
+	description varchar(255)
+);
+
+CREATE TABLE music(
+	music_id int NOT NULL,
+	title varchar(255),
+	description varchar(255),
+	price decimal(19,2),
+	album_id int,
+	category_id int, 
+	PRIMARY KEY (music_id)
+);
+
+CREATE TABLE genre(
+	genre_id int NOT NULL,
+	name varchar(255),
+	PRIMARY KEY (genre_id)
+);
+
+CREATE TABLE category(
+	genre_id int,
+	category_id int,
+	name varchar(255),
+	PRIMARY KEY (category_id)
+);
+
+ALTER TABLE music 
+ADD FOREIGN KEY (category_id) REFERENCES category(category_id);
+
+ALTER TABLE music 
+ADD FOREIGN KEY (album_id) REFERENCES album(album_id);
+
+ALTER TABLE category 
+ADD FOREIGN KEY (genre_id) REFERENCES genre(genre_id);
